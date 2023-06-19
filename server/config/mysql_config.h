f2079bf97a82c5d8a721e3d466a964a38900d46c
WPF196 <1962978635@qq.com>
2023-06-19 17:02:26 +0800

    :wrench:add config_file

---
+++ server/config/mysql_config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define HOST "127.0.0.1"
+#define USERNAME "root"
+#define PASSWORD "010418"
+#define PORT 3306
+#define DATABASE "chat_room"
---
+++ server/config/server_config.h
@@ -0,0 +1,5 @@
+#pragma
+
+#define HOST "127.0.0.1"
+#define PORT 9000
+#define MAX_CONNECTION 1024
