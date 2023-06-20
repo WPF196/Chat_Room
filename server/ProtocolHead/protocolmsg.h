42d1017fb75d39aa69dca2a0443bc5bbbf86b76f
WPF196 <1962978635@qq.com>
2023-06-20 14:44:48 +0800

    :construction:add protocolmsg

---
+++ server/ProtocolHead/protocolmsg.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// 协议宏 单位B
+// 头部
+#define HEAD_BUFFER_SIZE 8      // 头部
+#define PROTOCOL_ID_SIZE 1      // 协议
+#define ACCOUNT_SIZE 2          // 账户
+#define DATA_TYPE_SIZE 1        // 数据类型
+#define DATA_SIZE 4             // 数据大小
